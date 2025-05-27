62b61152cddc06d0764dd3cabde86d2b9288ba4d
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-27 13:51:45 -0400

    test three

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,49 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserRecord sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private UserRecord recipient;
+
+    @Column(nullable = false)
+    private float amount;
+
+    protected TransactionRecord() {
+    }
+
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public UserRecord getSender() {
+        return sender;
+    }
+
+    public UserRecord getRecipient() {
+        return recipient;
+    }
+}
+
---
+++ src/main/java/com/jpmc/midascore/repository/UserRecordRepository.java
@@ -0,0 +1,12 @@
+package com.jpmc.midascore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jpmc.midascore.entity.UserRecord;
+
+public interface UserRecordRepository extends JpaRepository<UserRecord, Long> {
+    Optional<UserRecord> findById(long id);
+}
+
